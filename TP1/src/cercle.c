f136ed345570aabeaa949b5122b07a1a264a2422
karlottam <rubydekarlotta@gmail.com>
2025-10-24 10:41:33 +0200

    m

--- TP1/src/cercle.c
+++ TP1/src/cercle.c
@@ -4,6 +4,6 @@
 int main() {
 double pi = 3.1416;
 double r = 56;  
-    printf("L'air du cercle = %f", pi*(r*r) + \n);
+    printf("L'air du cercle = %f", pi*(r*r) "\n");
    printf("Le périmètre du cercle = %f", pi*(r+r));
 }
